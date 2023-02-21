d653e1bc945547f04fb4a697abb7086e994a1cd2
monash1058 <monash.karthi@gmail.com>
2023-02-21 21:20:42 +0800

    changes commit

--- build/index.js
+++ build/index.js
@@ -7,12 +7,8 @@ const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("./src/middleware/express"));
 const config_1 = require("./src/config");
 const mongoose_1 = require("./src/connection/mongoose");
-const express_2 = __importDefault(require("./src/middleware/express"));
-const awsServerlessExpress = require("aws-serverless-express");
 const server = http_1.default.createServer(express_1.default);
 server.listen(config_1.config.PORT, () => {
     console.log(`Server is running on PORT ${config_1.config.PORT}`);
     (0, mongoose_1.connectDb)();
-    const servers = awsServerlessExpress.createServer(express_2.default);
-    return awsServerlessExpress.proxy(servers, "PROMISE").promise;
 });
