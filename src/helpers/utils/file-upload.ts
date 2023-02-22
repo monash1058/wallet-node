15024f993b77ea1a589546ad9094d02caf229237
monash1058 <monash.karthi@gmail.com>
2023-02-22 11:46:01 +0800

    aws changes config

--- src/helpers/utils/file-upload.ts
+++ src/helpers/utils/file-upload.ts
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const { config } = require("../config");
+const { config } = require("../../config");
 const aws = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
